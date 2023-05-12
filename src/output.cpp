0d7eadcf12e519ccc6e273da6c8147388aeac119
Peter Dupris <peter@dupris.us>
2023-05-11 19:34:24 -0700

    Experiment to output CBR mp3 @ 22050 Hz

--- src/output.cpp
+++ src/output.cpp
@@ -135,8 +135,8 @@ lame_t airlame_init(mix_modes mixmode, int highpass, int lowpass) {
 	}
 
 	lame_set_in_samplerate(lame, WAVE_RATE);
-	lame_set_VBR(lame, vbr_mtrh);
-	lame_set_brate(lame, 16);
+	// lame_set_VBR(lame, vbr_mtrh);
+	lame_set_brate(lame, 32);
 	lame_set_quality(lame, 7);
 	lame_set_lowpassfreq(lame, lowpass);
 	lame_set_highpassfreq(lame, highpass);
